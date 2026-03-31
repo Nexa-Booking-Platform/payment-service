68185bc3902ce9b38fb1f95640d10a970c75c76b
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-03-31 19:18:14 +0300

    Add Payment entity to Payment Service with JPA annotations and Lombok support

---
+++ src/main/java/com/bookingsystem/payment_service/entity/Payment.java
@@ -0,0 +1,28 @@
+package com.bookingsystem.payment_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "payments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String bookingId;
+    private String userId;
+
+    private double amount;
+    private String status;
+
+    private String providerRef;
+}
